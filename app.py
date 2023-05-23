bb89f7bfba52e8f59dcb13cc619a99a00386ea62
Leonardo Mejia <leonardo.mejia@quorumsoftware.com>
2023-05-23 13:44:10 -0500

    changed a list for a set for constant lookup times

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 print("please input the list of numbers separated by commas:")
 input_values = input()
-input_values = [int(x) for x in input_values.split(",")]
+input_values = {int(x) for x in input_values.split(",")}
 print("please input the target sum")
 target = int(input())
 
-scoped_values = []
+scoped_values = set()
 tuple_array = []
 for i in input_values:
     if 12 - i in scoped_values:
         tuple_array.append((i, 12-i))
-        scoped_values.append(i)
+        scoped_values.add(i)
     else:
-        scoped_values.append(i)
+        scoped_values.add(i)
 
 print("the pairs of numbers that add up to the target sum are:")
 for tuple in tuple_array:
