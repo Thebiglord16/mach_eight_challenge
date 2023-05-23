93dd724c775d78c9c3a888db6c40c4a0f0a67694
Leonardo Mejia <leonardo.mejia@quorumsoftware.com>
2023-05-23 16:49:29 -0500

    replace lingering 12 that worked as a test variable for target variable

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 print("please input the list of numbers separated by commas:")
-input_values = input()
-input_values = {int(x) for x in input_values.split(",")}
+tuple_array = []
+scoped_values = set()
+input_values = {int(x) for x in input().split(",")}
 print("please input the target sum")
 target = int(input())
 
-scoped_values = set()
+
 tuple_array = []
 for i in input_values:
     if target - i in scoped_values:
-        tuple_array.append((i, 12-i))
+        tuple_array.append((i, target-i))
         scoped_values.add(i)
     else:
         scoped_values.add(i)
